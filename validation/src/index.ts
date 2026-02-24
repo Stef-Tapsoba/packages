29c1f786a7715368a1bfb79f8b1bde1ffa495c7c
Stef-Tapsoba <93938621+Stef-Tapsoba@users.noreply.github.com>
2026-02-24 15:57:42 -0500

    Add match validator export to index.ts

--- validation/src/index.ts
+++ validation/src/index.ts
@@ -7,6 +7,7 @@ export * from "./validators/email"
 export * from "./validators/password"
 export * from "./validators/username"
 export * from "./validators/required"
+export * from "./validators/match"
 
 export * from "./schemas/login"
 export * from "./schemas/profile"
